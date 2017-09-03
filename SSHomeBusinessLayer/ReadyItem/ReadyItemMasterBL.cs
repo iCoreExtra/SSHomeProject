9d4b9ab85ca3330917f6c787fe536c45cb047bae
icoresandeep <patilsandeep16491@gmail.com>
2017-09-03 14:06:42 +0530

    Changes in ReadyItem Master

---
+++ SSHomeBusinessLayer/ReadyItem/ReadyItemMasterBL.cs
@@ -0,0 +1,53 @@
+﻿using SSHomeBusinessLayerTypes;
+using SSHomeCommon;
+using SSHomeDataModel;
+using SSHomeRepositoryTypes;
+using SSHomeRepositoryFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSHomeBusinessLayer
+{
+    public class ReadyItemMasterBL : BaseBusinessLayer, IReadyItemMasterBL
+    {
+        #region class members
+
+        private IReadyItemMasterRepository _repository;
+
+        #endregion private
+
+        #region Constructor
+        public ReadyItemMasterBL()
+        {
+            _repository = RepositoryFactory.GetRepository<IReadyItemMasterRepository>();
+        }
+
+        #endregion Constructor
+
+        #region IReadyItemMasterBL member
+        public Result<ReadyItemMaster> Add(ReadyItemMaster model)
+        {
+            return _repository.Add(model);
+        }
+
+        public Result<ReadyItemMaster> ReadyItemUpdate(ReadyItemMaster model)
+        {
+            return _repository.ReadyItemUpdate(model);
+        }
+
+
+        public IList<ReadyItemMaster> GetReadyItemListAll()
+        {
+            return _repository.GetReadyItemListAll();
+        }
+
+        public ReadyItemMaster GetReadyItemDetails(int readyitemId)
+        {
+            return _repository.GetReadyItemDetails(readyitemId);
+        }
+        #endregion
+    }
+}
---
+++ SSHomeBusinessLayerTypes/ReadyItem/IReadyItemMasterBL.cs
@@ -0,0 +1,21 @@
+﻿using SSHomeDataModel;
+using SSHomeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSHomeBusinessLayerTypes
+{
+    public interface IReadyItemMasterBL : IBusinessLayer
+    {
+        Result<ReadyItemMaster> Add(ReadyItemMaster model);
+
+        Result<ReadyItemMaster> ReadyItemUpdate(ReadyItemMaster model);
+
+        IList<ReadyItemMaster> GetReadyItemListAll();
+
+        ReadyItemMaster GetReadyItemDetails(int readyitemId);
+    }
+}
